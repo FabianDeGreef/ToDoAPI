1a4f0ae7ec9fc78d2cafa1f90fae2e281ce4b9c4
Fabian De Greef <fabian.degreef@student.pxl.be>
2019-03-16 01:00:51 +0100

    Initial Init
    Initial Init Project

---
+++ ToDoAPI/Controllers/ToDoController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Data;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public ToDoController(TodoContext context)
+        {
+            _context = context;
+
+            if(!_context.TodoItems.Any())
+            {
+                _context.TodoItems.AddRange(new ToDoItem {Name = "Item1"}, new ToDoItem { Name = "Item2" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/ToDo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetToDoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+     
+        // GET api/ToDo/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(long id)
+        {
+            var item = await _context.TodoItems.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        // POST api/ToDo
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetToDoItem), new {Id = item.Id}, item);
+        }
+        
+       
+
+        // PUT api/ToDo/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoItem(long id, ToDoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // DELETE api/ToDo/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDoItem(long id)
+        {
+            var toDoItem = await _context.TodoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(toDoItem);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
